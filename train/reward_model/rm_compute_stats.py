38b392659f237c31b9a4eef9803e722edef41edd
Anna Trella <annatrella@g.harvard.edu>
2023-09-29 16:12:45 +0000

    saving values and stats from reward model

--- train/reward_model/rm_compute_stats.py
+++ train/reward_model/rm_compute_stats.py
@@ -6,10 +6,11 @@ import sys
 
 grandparent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir))
 sys.path.append(grandparent_dir)
-
+from global_vars import MAIN_PATH
 from utils.model.model_utils import create_critic_model
 from utils.utils import to_device
 from utils.utils import load_hf_tokenizer
+
 from sklearn.metrics import f1_score
 
 def parse_args():
@@ -98,6 +99,8 @@ def get_data(input_path):
 
 def get_rewards(rm_model, tokenizer, device, prompt_list, good_ans_list, bad_ans_list):
     pred_labels = []
+    chosen_rewards = []
+    rejected_rewards = []
     for prompt, good_ans, bad_ans in zip(prompt_list, good_ans_list,
                                          bad_ans_list):
         batch = prepare_datapair(prompt,
@@ -110,10 +113,14 @@ def get_rewards(rm_model, tokenizer, device, prompt_list, good_ans_list, bad_ans
         # Run inference
         with torch.no_grad():
             outputs = rm_model(**batch)
-        predicted_label = outputs["chosen_mean_scores"].item() > outputs["rejected_mean_scores"].item()
+        chosen_reward = outputs["chosen_mean_scores"].item()
+        rejected_reward = outputs["rejected_mean_scores"].item()
+        predicted_label = chosen_reward > rejected_reward
+        chosen_rewards.append(chosen_reward)
+        rejected_rewards.append(rejected_reward)
         pred_labels.append(predicted_label)
 
-    return pred_labels
+    return pred_labels, chosen_rewards, rejected_rewards
 
 def run_pair_comparison():
     args = parse_args()
@@ -126,24 +133,54 @@ def run_pair_comparison():
     rm_model.eval()
     prompt_list_train, good_ans_list_train, bad_ans_list_train, prompt_list_dev, good_ans_list_dev, bad_ans_list_dev = get_data(args.data_path)
 
-    pred_labels_train = get_rewards(rm_model, tokenizer, device, prompt_list_train, good_ans_list_train, bad_ans_list_train)
-    pred_labels_dev = get_rewards(rm_model, tokenizer, device, prompt_list_dev, good_ans_list_dev, bad_ans_list_dev)
+    pred_labels_train, chosen_rewards_train, rejected_rewards_train = get_rewards(rm_model, tokenizer, device, prompt_list_train, good_ans_list_train, bad_ans_list_train)
+    pred_labels_dev, chosen_rewards_dev, rejected_rewards_dev = get_rewards(rm_model, tokenizer, device, prompt_list_dev, good_ans_list_dev, bad_ans_list_dev)
 
-    return pred_labels_train, pred_labels_dev
+    return pred_labels_train, chosen_rewards_train, rejected_rewards_train, pred_labels_dev, chosen_rewards_dev, rejected_rewards_dev
 
 
 def compute_f1_score(predicted_labels, true_labels):
     f1 = f1_score(true_labels, predicted_labels)
     return f1
 
+def compute_mean_var_reward(chosen_rewards, rejected_rewards, dataset_type):
+    chosen_rewards = np.array(chosen_rewards)
+    rejected_rewards = np.array(rejected_rewards)
+    abs_diff = np.abs(chosen_rewards - rejected_rewards)
+
+    print("Avg. Reward Absolute Difference \n")
+    print(f"{dataset_type}: {np.mean(abs_diff):.3f} ({np.var(abs_diff):.3f})")
+
 if __name__ == "__main__":
-    pred_labels_train, pred_labels_dev = run_pair_comparison()
+    pred_labels_train, chosen_rewards_train, rejected_rewards_train, pred_labels_dev, chosen_rewards_dev, rejected_rewards_dev = run_pair_comparison()
+    ### SAVING REWARDS ###
+    train_obj = {
+        "chosen_rewards": chosen_rewards_train,
+        "rejected_rewards": rejected_rewards_train,
+        "labels": pred_labels_train
+    }
+    with open(MAIN_PATH + "/model_outputs/rm-eval/rewards_train.json", 'w') as json_file:
+        json.dump(train_obj, json_file, indent=4)
+    val_obj = {
+        "chosen_rewards": chosen_rewards_dev,
+        "rejected_rewards": rejected_rewards_dev,
+        "labels": pred_labels_dev
+    }
+    with open(MAIN_PATH + "/model_outputs/rm-eval/rewards_val.json", 'w') as json_file:
+        json.dump(val_obj, json_file, indent=4)
+
+    ### COMPUTING STATS ###
     true_labels_train = [1] * len(pred_labels_train)
     true_labels_dev = [1] * len(pred_labels_dev)
     print("Number of Datapoints (Train):", len(pred_labels_train))
     print("Number of Datapoints (Validation):", len(pred_labels_dev))
 
+    # F1 Scores
     f1_score_train = compute_f1_score(pred_labels_train, true_labels_train)
     f1_score_dev = compute_f1_score(pred_labels_dev, true_labels_dev)
     print("F1 Score (Train):", f1_score_train)
     print("F1 Score (Validation):", f1_score_dev)
+
+    # Average and Var Reward
+    compute_mean_var_reward(chosen_rewards_train, rejected_rewards_train, "Train")
+    compute_mean_var_reward(chosen_rewards_dev, rejected_rewards_dev, "Val")
--- train/reward_model/rm_compute_stats.sh
+++ train/reward_model/rm_compute_stats.sh
@@ -1,3 +1,3 @@
-python  rm_f1_scores.py \
+python  rm_compute_stats.py \
     --model_name_or_path /personalizing-llm-hl/model-checkpoint/reward-model \
     --data_path /personalizing-llm-hl/data/rm_baseline_data.json
